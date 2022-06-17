c31eca5d6a99811c936d839e36461144a0ccb90c
timleungtech <hellopwntchu@gmail.com>
2022-06-17 12:12:06 -0400

    change id var

--- server.js
+++ server.js
@@ -7,37 +7,44 @@ require('dotenv').config()
 app.use(express.json())
 let db,
     DB_STRING = process.env.DB_STRING,
-    dbName = 'phonebook'
+    dbName = 'phonebook',
+    personsCollection
 
 MongoClient.connect(DB_STRING, { useUnifiedTopology: true })
     .then(client => {
-        console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
+        personsCollection = db.collection('persons')
+        console.log(`Connected to ${dbName} Database`)
 })
 
 let persons = [
     { 
-      "id": 1,
+      "_id": 0,
+      "name": "unknown", 
+      "number": "000-000-0000"
+    },
+    { 
+      "_id": 1,
       "name": "Sally Smith", 
       "number": "111-111-1111"
     },
     { 
-      "id": 2,
+      "_id": 2,
       "name": "Joe Johnson", 
       "number": "222-222-2222"
     },
     { 
-      "id": 3,
+      "_id": 3,
       "name": "Wayne Williams", 
       "number": "333-333-3333"
     },
     { 
-      "id": 4,
+      "_id": 4,
       "name": "Bobby Brown", 
       "number": "444-444-4444"
     },
     { 
-      "id": 5,
+      "_id": 5,
       "name": "Grace Garcia", 
       "number": "555-555-5555"
     }  
@@ -55,14 +62,14 @@ app.get('/api/persons/', (req, res) => {
     res.json(persons)
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const personId = req.params.id
+app.get('/api/persons/:_id', (req, res) => {
+    const personId = req.params._id
     if ( persons[personId] ){
         res.json(persons[personId])
     } else {
         // res.json(persons[0])
         return res.status(400).json({ 
-                "id": 'unknown',
+                "_id": 'unknown',
                 "name": "unknown", 
                 "number": "000-000-0000"
         })

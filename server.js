6005dcc8a510355aa680d8ed3d7334f87eb6651d
timleungtech <hellopwntchu@gmail.com>
2022-06-17 12:16:53 -0400

    remove id property

--- server.js
+++ server.js
@@ -19,32 +19,26 @@ MongoClient.connect(DB_STRING, { useUnifiedTopology: true })
 
 let persons = [
     { 
-      "_id": 0,
       "name": "unknown", 
       "number": "000-000-0000"
     },
     { 
-      "_id": 1,
       "name": "Sally Smith", 
       "number": "111-111-1111"
     },
     { 
-      "_id": 2,
       "name": "Joe Johnson", 
       "number": "222-222-2222"
     },
     { 
-      "_id": 3,
       "name": "Wayne Williams", 
       "number": "333-333-3333"
     },
     { 
-      "_id": 4,
       "name": "Bobby Brown", 
       "number": "444-444-4444"
     },
     { 
-      "_id": 5,
       "name": "Grace Garcia", 
       "number": "555-555-5555"
     }  
@@ -62,19 +56,18 @@ app.get('/api/persons/', (req, res) => {
     res.json(persons)
 })
 
-app.get('/api/persons/:_id', (req, res) => {
-    const personId = req.params._id
-    if ( persons[personId] ){
-        res.json(persons[personId])
-    } else {
-        // res.json(persons[0])
-        return res.status(400).json({ 
-                "_id": 'unknown',
-                "name": "unknown", 
-                "number": "000-000-0000"
-        })
-    }
-})
+// app.get('/api/persons/:_id', (req, res) => {
+//     const personId = req.params._id
+//     if ( persons[personId] ){
+//         res.json(persons[personId])
+//     } else {
+//         return res.status(400).json({ 
+//                 "_id": 'unknown',
+//                 "name": "unknown", 
+//                 "number": "000-000-0000"
+//         })
+//     }
+// })
 
 // app.post('/api/addPerson', (req, res) => {
 //     collection('persons').insertOne({id: req.body.id, name: req.body.name})
